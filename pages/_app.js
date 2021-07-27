e50f31c6d6457155ba7f6bf69652215aa8d8e70e
harsh vats <harsh@xamidea.in>
2021-07-27 22:31:12 +0530

    ignore

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css"; // Import the CSS
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import styled from "styled-components";
 // import AOS from "aos";
 // import "aos/dist/aos.css";
 config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
