85c4c27c4b7286a6e1eecf52c957387f05751ba9
harsh vats <harsh@xamidea.in>
2021-08-11 10:34:31 +0530

    video added below aim section

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,46 @@
 import Head from "next/head";
-import Image from "next/image";
-import styled from "styled-components";
 import About from "../components/About";
 import Facilities from "../components/Facilities";
 import HomepageBanner from "../components/HomepageBanner";
 import Vision from "../components/Vision";
 import Aim from "../components/Aim";
 import WhyUs from "../components/WhyUs";
+import styled from "styled-components";
 
 export default function Home() {
   return (
-    <div>
+    <>
+      <Head>
+        <title>Best Pre School Concept in India - Concept ABC</title>
+      </Head>
       <HomepageBanner />
       <About />
       <Vision />
       <Aim />
+      <VideoWrapper>
+        <video controls>
+          <source src="/static/about.mp4" />
+        </video>
+      </VideoWrapper>
       <Facilities />
       <WhyUs />
-    </div>
+    </>
   );
 }
+
+const VideoWrapper = styled.div`
+  background: #ffce00;
+  padding: 60px 0;
+  text-align: center;
+  video {
+    width: 800px;
+    border-radius: 5px;
+    box-shadow: 0 0 20px 0 rgb(0 0 0 / 40%);
+  }
+
+  @media (max-width: 768px) {
+    video {
+      width: 96%;
+    }
+  }
+`;
